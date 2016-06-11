3dc44cd0ef7eeb932ab609ffa9f65ac117cf7fee
Naval <navalnovel@gmail.com>
2016-06-11 20:13:08 +0530

    fixes in koa route (we needed version 2 for working with async keyword)

--- src/api/users.js
+++ src/api/users.js
@@ -1,13 +1,16 @@
 import * as usersModule from "../domain/users";
 
 export async function getTopActiveUsers(ctx) {
-  const page = (this.params.page) ? this.params.page : 0;
-  
-  const { users, error, code } = await usersModule.getTopActiveUsers(page);
-  
-  if (user) ctx.body = { user };
-  if (message) ctx.message = message;
-  ctx.status = code;
+  const page = (this.request.query.page) ? this.request.query.page : 0;
+ 
+  try { 
+    const users = await usersModule.getTopActiveUsers(page);
+    ctx.body = { users };
+  } catch(err) {
+    ctx.body = { message: err.message };
+    ctx.status = err.status || 500;
+  }
+
 }
 
 
@@ -15,14 +18,16 @@ export async function getUserDetails(ctx) {
   const id = this.params.id;
 
   if (!id) {
-    this.response.status = 400;
-    this.response.message = "url should have an `id` parameter";
-    return;
+    ctx.body = { message: "id parameter is required in getUserDetails api" };
+    ctx.status = err.status || 400;
+  } 
+  
+  try {
+    const user = await usersModule.getUserDetails(id);
+    ctx.body = { user };
+  } catch(err) {
+    ctx.body = { message: err.message };
+    ctx.status = err.status || 500;
   }
 
-  const { user, message, code } = await usersModule.getUserDetails(id);
-
-  if (user) ctx.body = { user };
-  if (message) ctx.message = message;
-  ctx.status = code;
 }
--- src/app.js
+++ src/app.js
@@ -7,8 +7,12 @@ import * as users from "./api/users";
 
 const app = new koa();
 
-app.use(router.get('/topActiveUsers', users.topActiveUsers));
-app.use(router.get('/users', users.getUserDetails));
+app.use(router.get( '/topActiveUsers', users.getTopActiveUsers ));
+app.use(router.get( '/users', users.getUserDetails ));
+
+app.on("error", error => {
+  console.error(error.stack);
+});
 
 const port = process.argv.length >= 3 ? process.argv[2] : 8000;
 app.listen(port);
