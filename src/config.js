414b13705444f84cf1675a8b343c803cee02a112
Naval <navalnovel@gmail.com>
2016-06-12 14:38:47 +0530

    created a different user for this project

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 
-export const db_url = process.env.DB_URL || "postgres://transitdbuser:biglambda@localhost/jobsdb"; // TODO create a new user
+export const db_url = process.env.DB_URL || "postgres://jobsdbuser:anonymous@localhost/jobsdb"; // TODO create a new user
 
